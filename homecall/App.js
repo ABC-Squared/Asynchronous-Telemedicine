8dd14854b45cd076bf282cb8f2b0c391a1bc8c42
Bradon Thymes <bmt.wcpa@gmail.com>
2022-02-24 23:37:47 -0500

    Update App.js

--- homecall/App.js
+++ homecall/App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Appointment from './components/Appointment';
+//import { createStackNavigator } from 'react-navigation'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Title for upcoming appointments */}
+      <View style={styles.upcomingWrapper}>
+        <Text style={styles.sectionTitle}>Upcoming Appointments</Text>
+
+        <View style={styles.upcomingItems}>
+          {/* This is where the upcoming appointments will go */}
+          <Appointment text={'Check-Up'} />
+          <Appointment text={'Physical'}/>
+          <Appointment text={'Lab Results'}/>
+        </View>
+      </View>
+
+      {/* Title for upcoming appointments */}
+      <View style={styles.recentWrapper}>
+        <Text style={styles.sectionTitle}>Recent Appointments</Text>
+
+        <View style={styles.recentItems}>
+          {/* This is where the upcoming appointments will go */}
+          <Appointment text={'Follow-Up (Non-Urgent)'} />
+          <Appointment text={'Lab Results'}/>
+          <Appointment text={'Check-Up'}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +37,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  upcomingWrapper: {
+    paddingTop: 120,
+    paddingHorizontal: 20
+  },
+  sectionTitle: {
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  upcomingItems: {
+    marginTop: 20,
+    marginHorizontal: 10,
+    padding: 10,
+    backgroundColor: '#F2F2F2',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,  
+    elevation: 3
+  },
+  recentWrapper: {
+    paddingTop: 25,
+    paddingHorizontal: 20
+  },
+  recentItems: {
+    marginTop: 20,
+    marginHorizontal: 10,
+    padding: 10,
+    backgroundColor: '#F2F2F2',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,  
+    elevation: 3
   },
 });
