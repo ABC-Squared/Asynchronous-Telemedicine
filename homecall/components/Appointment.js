4772b6618c3d7f15353412e4b7ac2cbcea2ca6ac
Bradon Thymes <bmt.wcpa@gmail.com>
2022-02-24 23:37:50 -0500

    Create Appointment.js

---
+++ homecall/components/Appointment.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Appointment = (props) => {
+
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemText}>
+                <Text>{props.text}</Text>
+            </View>
+            <View style={styles.chatButton}></View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#DBD9D9',
+        padding: 15,
+        borderRadius: 10,
+        margin: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,  
+        elevation: 3
+    },
+    chatButton:{
+        width: 12,
+        height: 12,
+        backgroundColor: '#F75050',
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    itemText:{
+        
+    },
+});
+
+export default Appointment;
