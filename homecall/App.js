7a523d910c967911cb798326d04ce78a040dacf0
Bradon Thymes <bmt.wcpa@gmail.com>
2022-04-13 11:47:05 -0400

    Update App.js

--- homecall/App.js
+++ homecall/App.js
@@ -1,13 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import BottomNavigator from './navigation/TabNavigation';
-import { LoginStackNavigator } from './navigation/StackNavigation';
+import { LoginStackNavigator, MainStackNavigator } from './navigation/StackNavigation';
 
 export default function App() {
   // For testing, false -> show the logging in stack; true -> show the main stack
   var isLoggedIn = true;
   return (
     <NavigationContainer>
-      {isLoggedIn ? <BottomNavigator /> : <LoginStackNavigator /> }
+      <BottomNavigator />
     </NavigationContainer>
   );
 }
