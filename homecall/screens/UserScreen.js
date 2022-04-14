bb539f7b1acda537b5133d4df1e971278ad9a8a0
Cassey <casseyanene@gmail.com>
2022-04-13 23:44:00 -0400

    firebase can pull profile information

--- homecall/screens/UserScreen.js
+++ homecall/screens/UserScreen.js
@@ -1,19 +1,60 @@
-import { color } from '@rneui/base';
-import {StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import firebase from 'firebase/compat/app';
+import { useState, useEffect } from 'react';
 
 
 function UserScreen() {
+  let currentUserUID = firebase.auth().currentUser.uid;
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [address, setAddress] = useState('');
+
+  useEffect(() => {
+    async function getUserInfo(){
+      let doc = await firebase
+      .firestore()
+      .collection('patients')
+      .doc(currentUserUID)
+      .get();
+
+      if (!doc.exists){
+        Alert.alert('No user data found!')
+      } else {
+        let dataObj = doc.data();
+        setFirstName(dataObj.firstName)
+        setLastName(dataObj.lastName)
+        setAddress(dataObj.address)
+      }
+    }
+    getUserInfo();
+  })
+
   return(
     <View style={styles.container}>
       {/* Title for upcoming appointments */}
       <View style={styles.upcomingWrapper}>
-        <Text style={styles.sectionTitle}>MITCHELL, Abby</Text>
+        <Text style={styles.helloTextStyle}>Hello, {firstName}!</Text>
+
+      {/* Image of Patient */}
+      <Image style={styles.imageContainer}
+        source={require(`../assets/profilepictures/testpfp.jpg`)} //Add other pfp to here.
+      />
+
+      {/* Name of Patient */}
+      <View style={styles.profileContainer}>
+        <Text style={styles.nameTextStyle}>{firstName} {lastName}</Text>
+        </View>
+
+      {/* Name of Patient */}
+    <View style={styles.profileContainer}>
+      <Text style={styles.nameTextStyle}>{address}</Text>
+      </View>
+
 
         <View style={styles.upcomingItems}>
-          {/* This is where the upcoming appointments will go */}
-          <Text>Chronic Diagnoses</Text>
-          <Text>Previous Acute Diagnoses</Text>
-          <Text>View all Previous Diagnoses</Text>
+          {/* This is where the Meds and Immunization will go */}
+          <Text>Meds</Text>
+          <Text>Immunization</Text>
           
         </View>
       </View>
@@ -24,9 +65,40 @@ function UserScreen() {
 export default UserScreen;
 
 const styles = StyleSheet.create({
+    profileContainer : {
+      backgroundColor: '#F75050',
+      width: 300,
+      borderRadius: 10,
+      alignSelf: 'center',
+      height: 60,
+      marginTop: 30
+    },
+    nameTextStyle : {
+      color: 'white',
+      fontSize: 30,
+      alignSelf: 'center',
+      paddingTop: 10
+    },
+    helloTextStyle : {
+      alignSelf: 'center',
+      fontWeight: 'bold',
+      fontSize: 50
+    },
+    imageContainer : {
+      marginTop: 20,
+      marginHorizontal: 10,
+      padding: 10,
+      borderRadius: 10,
+      shadowOpacity: 0.3,
+      shadowRadius: 5,  
+      elevation: 3,
+      height: 232,
+      width: 232,
+      alignSelf: 'center'
+    },
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#fff'
     },
     upcomingWrapper: {
       paddingTop: 50,
