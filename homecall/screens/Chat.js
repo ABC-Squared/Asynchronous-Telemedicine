ee0be87ac0b6b54da6063a0ebab1d4e6f1796dce
Cassey <casseyanene@gmail.com>
2022-04-14 20:03:12 -0400

    user can send a message

--- homecall/screens/Chat.js
+++ homecall/screens/Chat.js
@@ -1,8 +1,50 @@
-import React from "react";
-import { GiftedChat } from "react-native-gifted-chat";
+import React, { useState, useEffect, useCallback } from "react";
+import { GiftedChat, Bubble} from "react-native-gifted-chat";
+// Docs: https://stackoverflow.com/questions/60205950/how-to-customize-react-native-gifted-chat-in-react-native-0-61
 
 export default function Chat() {
+    const [messages, setMessages] = useState([]);
+
+    // TODO: Custimize the Bubble
+    const customBubble = props => {
+        return (
+          <Bubble
+            {...props}
+            containerStyle={{
+              backgroundColor: "#F75050",
+            }}
+          />
+        );
+      };
+
+    useEffect(() => {
+        setMessages([
+          {
+            _id: 1,
+            text: 'Hi Abby, how are you can you explain more about your symptoms?',
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: 'Dr. Michelle Williams',
+              avatar: require(`../assets/profilepictures/drmichellewilliamspfp.png`),
+            },
+          },
+        ])
+      }, [])
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
+
     return (
-        <GiftedChat />
+        <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+            _id: 1,
+        }}
+        renderBubble={props => customBubble(props)}
+        />
     )
 }
